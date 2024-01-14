627dcecbab9a8a1251c336b1ebbaaf6db5feb186
Shubham-Kumar-cmd <kumar53shubham@gmail.com>
2024-01-14 17:12:29 +0530

    added centralize common configuration

--- swiggy-app/src/main/java/com/shubham/client/RestaurantServiceClient.java
+++ swiggy-app/src/main/java/com/shubham/client/RestaurantServiceClient.java
@@ -2,15 +2,27 @@ package com.shubham.client;
 
 import com.shubham.dto.OrderResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@RefreshScope
 public class RestaurantServiceClient {
+
+    @Value("${microservice.restaurant-service.endpoints.fetchOrderById.uri}")
+    private String fetchOrderUri;
+
+    @Value("${test.input}")
+    private String testValue;
+
     @Autowired
     private RestTemplate template;
 
     public OrderResponseDTO fetchOrderStatus(String orderId) {
-        return template.getForObject("http://RESTAURANT-SERVICE/restaurant/orders/status/" + orderId, OrderResponseDTO.class);
+        System.out.println("*************** "+testValue);
+        System.out.println("fetchOrderUri : "+fetchOrderUri);
+        return template.getForObject(fetchOrderUri + orderId, OrderResponseDTO.class);
     }
 }
